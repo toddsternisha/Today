e009863998e3786efa1800ea2d6aa4f0620651ce
toddsternisha <todds@me.com>
2022-05-20 11:31:15 -0400

    Initial Commit

---
+++ Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Todd Sternisha on 5/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
