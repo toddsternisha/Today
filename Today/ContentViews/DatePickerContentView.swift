72ad52d534784823f39f6cca527fd95388d866c7
toddsternisha <todds@me.com>
2022-05-22 13:53:07 -0400

    Completion of Using Content Views

---
+++ Today/ContentViews/DatePickerContentView.swift
@@ -0,0 +1,51 @@
+//
+//  DatePickerContentView.swift
+//  Today
+//
+//  Created by Todd Sternisha on 5/22/22.
+//
+
+import UIKit
+
+class DatePickerContentView: UIView, UIContentView {
+    struct Configuration: UIContentConfiguration {
+        var date = Date.now
+        
+        func makeContentView() -> UIView & UIContentView {
+            return DatePickerContentView(self)
+        }
+    }
+    
+    let datePicker = UIDatePicker()
+    var configuration: UIContentConfiguration {
+        didSet {
+            configure(configuration: configuration)
+        }
+    }
+    override var intrinsicContentSize: CGSize {
+        CGSize(width: 0, height: 44)
+    }
+    
+    init(_ configuration: UIContentConfiguration) {
+        self.configuration = configuration
+        super.init(frame: .zero)
+        addPinnedSubview(datePicker)
+        datePicker.preferredDatePickerStyle = .inline
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(configuration: UIContentConfiguration) {
+        guard let configuration = configuration as? Configuration else { return }
+        datePicker.date = configuration.date
+    }
+}
+
+extension UICollectionViewListCell {
+    func datePickerConfiguration() -> DatePickerContentView.Configuration {
+        DatePickerContentView.Configuration()
+    }
+
+}
---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by Todd Sternisha on 5/22/22.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
