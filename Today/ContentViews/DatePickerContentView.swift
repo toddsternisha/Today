a7e3624d9e685df2c48f69a86f1594f48c12251e
toddsternisha <todds@me.com>
2022-05-22 14:50:13 -0400

    Completion of Editing Reminders

--- Today/ContentViews/DatePickerContentView.swift
+++ Today/ContentViews/DatePickerContentView.swift
@@ -10,6 +10,7 @@ import UIKit
 class DatePickerContentView: UIView, UIContentView {
     struct Configuration: UIContentConfiguration {
         var date = Date.now
+        var onChange: (Date)->Void = { _ in }
         
         func makeContentView() -> UIView & UIContentView {
             return DatePickerContentView(self)
@@ -30,6 +31,7 @@ class DatePickerContentView: UIView, UIContentView {
         self.configuration = configuration
         super.init(frame: .zero)
         addPinnedSubview(datePicker)
+        datePicker.addTarget(self, action: #selector(didPick(_:)), for: .valueChanged)
         datePicker.preferredDatePickerStyle = .inline
     }
     
@@ -40,6 +42,11 @@ class DatePickerContentView: UIView, UIContentView {
     func configure(configuration: UIContentConfiguration) {
         guard let configuration = configuration as? Configuration else { return }
         datePicker.date = configuration.date
+    }
+    
+    @objc private func didPick(_ sender: UIDatePicker) {
+        guard let configuration = configuration as? DatePickerContentView.Configuration else { return }
+        configuration.onChange(sender.date)
     }
 }
 
